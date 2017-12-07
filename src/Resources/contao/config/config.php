6921cfe6a43c8f1ecdc5a2ec8fef6546af96c19a
Sascha Weidner <support@sioweb.de>
2017-12-07 12:39:14 +0100

    Init

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ */
+
+/**
+* @file config.php
+* @author Sascha Weidner
+* @version 4.0.0
+* @package sioweb.contao.extensions.backend
+* @copyright Sioweb - Sascha Weidner
+*/
+
+$GLOBALS['TL_CTE']['files']['sw_downloadfolder'] = 'Sioweb\DownloadFolder';
